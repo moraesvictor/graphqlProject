5753eee4c5227ca5e9323e5c9ccbf73c2a34a2cf
moraesvictor <moraesvictor@gmail.com>
2021-09-22 20:57:32 -0300

    feat(fsp): create entities and post a title

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { MikroORM } from '@mikro-orm/core';
 import * as C from './constants';
+import { Post } from './entities/Post';
 
 const main = async () => {
   const orm = await MikroORM.init({
@@ -8,6 +9,8 @@ const main = async () => {
     type: 'postgresql',
     debug: !C.__prod__
   });
+
+ orm.em.create(Post, {title: 'My first post '})
  
 }
 
