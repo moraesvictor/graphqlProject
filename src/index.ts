3f49b09606894343c38b40c0e8718c8aa7aa1448
moraesvictor <moraesvictor@gmail.com>
2021-09-22 15:55:09 -0300

    feat(fsp): add mikro-orm config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello-there")
+import { MikroORM } from '@mikro-orm/core';
+import * as C from './constants';
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: 'fullstack-project',
+    type: 'postgresql',
+    debug: !C.__prod__
+  });
+ 
+}
+
+main();
